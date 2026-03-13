c849f918a2c318c8dfde0f1c0a7a519e695ed331
natanael <rojonatanael99@gmail.com>
2026-03-13 11:28:53 -0400

    feat(Users): Add password change done view to user management URLs

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -35,6 +35,11 @@ urlpatterns = [
         name="password_change",
     ),
     path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+    ),
+    path(
         "password_reset/done/",
         auth_views.PasswordResetDoneView.as_view(),
         name="password_reset_done",
